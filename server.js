1df14ceef534cf73bec012e57d39b1f6478dab6c
dalchek <dalchek999@gmail.com>
2023-12-09 13:02:05 +0100

    Aded EDIT button and functionality

--- server.js
+++ server.js
@@ -40,6 +40,26 @@ app.post('/save', (req, res) => {
         }
     );
 });
+
+app.get('/edit_acc/:id', (req,res)=>{
+    let id = req.params.id
+    db.accounts.findOne({"_id":db.ObjectId(id)},(err,data)=>{
+        res.render('edit-form-view',{data:data})
+    })
+})
+
+app.post('/edit_acc_db',(req,res)=>{
+    let id = req.body.id
+    db.accounts.update({"_id":db.ObjectId(id)}, {$set:{
+        first_name : req.body.first_name,
+        last_name : req.body.last_name,
+        email : req.body.email,
+        phone : req.body.phone
+    }},(err,data)=>{
+        res.redirect('/')
+    })
+})
+
 app.listen(3000, () => {
     console.log('Listening to port 3000');
 });
